0b8d7086cc09908950efdea160c2ecc74ad1a29a
vuphuclam98 <vunaph08450@fpt.edu.vn>
2021-01-22 13:00:02 +0700

    add file giao dien

---
+++ assets/js/styles.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+    $(".show__menu").click(function(){
+        $(".header-nav,.roat").toggleClass("active");
+    });
+
+    $(".close__menu,.roat").click(function(){
+        $(".header-nav,.roat").removeClass("active");
+    });
+
+    /* ẩn phần thừa khi click slide products */
+    $(".btn__next,.btn__prev").click(function(){
+        $(".carousel-inner").removeClass("overflow-hiden");
+    });
+
+    $(".product-item").hover(function(){
+        $(".carousel-inner").addClass("overflow-hiden");
+    });
+    /* dropdown menu  */
+    $("#btn-cate__show").click(function(){
+        $("#dropdown-categories").toggleClass("show");
+    });
+    /* carousel */
+    $('.carousel').carousel({
+        pause: true
+    });
+
+    $(".top-menu__item").click(function(){
+        $(this).toggleClass('show');
+        $(this).siblings().toggleClass('hiden');
+    });
+});
+
